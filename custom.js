3dfee2457e2b50cf0a454886875a39c90d3aa25d
echoflove <105045250+echoflove@users.noreply.github.com>
2022-12-16 14:09:11 +0800

    Update custom.js

--- custom.js
+++ custom.js
@@ -2,6 +2,7 @@ var myNav = document.getElementById('header');
 window.onscroll = function () { 
     if (document.body.scrollTop >= 100 ) {
          $(".App-header").css('background-color', '#ffffff');
+         $(".FormControl").css('background-color', '#ffffff');
     } 
     else {
         myNav.classList.remove("nav-scrolled");
