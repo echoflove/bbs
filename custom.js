55701ba16e91a55370a4421d1b6ad5ed3e81c613
echoflove <105045250+echoflove@users.noreply.github.com>
2022-12-16 13:59:20 +0800

    Update custom.js

--- custom.js
+++ custom.js
@@ -1,8 +1,9 @@
-const navbar = document.querySelector('.App-header');
-window.onscroll = () => {
-    if (window.scrollY > 100) {
-        navbar.classList.add('nav-scrolled');
-    } else {
-        navbar.classList.remove('nav-scrolled');
+var myNav = document.getElementById('header');
+window.onscroll = function () { 
+    if (document.body.scrollTop >= 100 ) {
+        myNav.classList.add("nav-scrolled");
+    } 
+    else {
+        myNav.classList.remove("nav-scrolled");
     }
 };
